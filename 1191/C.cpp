f8707f2374310ebb9c3073c554887cbe130f1230
Mahdi Alikhasi <mahdialikhasi1389@gmail.com>
2019-09-03 18:02:29 +0430

    Contest 1191
    Bug fix and solve new problem from this contest

--- 1191/C.cpp
+++ 1191/C.cpp
@@ -5,17 +5,18 @@ using namespace std;
 #include <vector>
 
 int main(){
-	int n, m, k;
+	long long int n, m, k;
 	cin >> n >> m >> k;
-	vector<int> data(m);
+	vector<long long int> data(m);
 	for (int i = 0; i < m; ++i){
 		cin >> data[i];
 		data[i]--;
 	}
-	int page = data[0] / k;
-	int removes = 0;
-	int d = 0;
-	int count = 1;
+	sort(data.begin(), data.end());
+	long long int page = data[0] / k;
+	long long int removes = 0;
+	long long int d = 0;
+	long long int count = 1;
 	for (int i = 0; i < m; ++i){
 		if((data[i] - d) / k == page){
 			removes++;
--- 1191/D.cpp
+++ 1191/D.cpp
@@ -3,39 +3,58 @@ using namespace std;
 #include <algorithm>
 #include <stdio.h>
 #include <vector>
-#include <string>
 int main(){
-	int n, k;
-	cin >> n >> k;
-	string a;
-	cin >> a;
-
-	
-	int x = 0;
-	int y = 0;
+	int n;
+	cin >> n;
+	vector<long long int> a(n);
 	for (int i = 0; i < n; ++i){
-		if(a[i] != a[0])
-			break;
-		else
-			x++;
+		cin >> a[i];
 	}
-	for (int i = n - 1; i >= 0; --i){
-		if(a[i] != a[n - 1])
+	sort(a.begin(), a.end());
+	
+	// check all zero
+	int st = 0;
+	for (int i = 0; i < n; ++i){
+		if(a[i] != 0){
+			st = 1;
 			break;
-		else
-			y++;
+		}
 	}
-
-	if(x + k >= n || y + k >= n || (x + y + k >= n && a[0] == a[n - 1])){
-		cout << "tokitsukaze" << endl;
+	if(!st){
+		cout << "cslnb" << endl;
 		return 0;
 	}
-	if(x == n-k-1 && y == n-k-1 && a[0] != a[n-1] && k >= (n+1)/2){
-		cout << "quailty" << endl;
-	}else{
-		cout << "once again" << endl;
+	// check double
+	int count = 1;
+	int countTWO = 0;
+	for (int i = 1; i < n; ++i)
+	{
+		if(a[i] == a[i - 1])
+			count++;
+		else
+			count = 1;
+		if(count == 2)
+			countTWO++;
+		if(count > 2)
+			break;
 	}
-	
-	
+	if(count > 2 || countTWO > 1){
+		cout << "cslnb" << endl;
+		return 0;
+	}
+	long long int ansi = 0;
+	int turn = 1;
+	for (int i = 0; i < n; ++i){
+		ansi = a[i] - i;
+		if(ansi < 0 || (i >= 2 && a[i] == a[i - 1] && a[i] == a[i - 2] + 1)){
+			cout << "cslnb" << endl;
+			return 0;
+		}
+		turn ^= ansi % 2;
+	}
+	if(turn == 1)
+		cout << "cslnb" << endl;
+	else
+		cout << "sjfnb" << endl;
 	return 0;
 }
